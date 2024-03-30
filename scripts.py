9cbe6bd07f1a22333c3413ffc911e6c2388f46c5
MD RONY GOLDER <ronygolderku@gmail.com>
2024-03-31 00:43:13 +0800

    update

--- scripts.py
+++ scripts.py
@@ -4,6 +4,18 @@ import copernicusmarine
 username = os.getenv('COPERNICUS_USERNAME')
 password = os.getenv('COPERNICUS_PASSWORD')
 
+aws_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+session = boto3.Session()
+
+# Create an S3 client
+s3_client = session.client('s3', endpoint_url='https://projects.pawsey.org.au')
+
+# Specify the S3 bucket and folder
+bucket_name = 'wamsi-westport-project-1'
+s3_folder = 'SH20221201_Westport_Deliverables/Raw_Data/Virtual_Sensor/'
+
 # Remotely open the whole dataset
 data = copernicusmarine.open_dataset(
     dataset_id="cmems_obs-oc_glo_bgc-plankton_my_l4-gapfree-multi-4km_P1D",
@@ -36,8 +48,16 @@ data = copernicusmarine.open_dataset(
     username=username,
     password=password,
 )
+df = data.to_dataframe().reset_index()
+csv_file_path = f'CHL_{last_date}.csv'
+df.to_csv(csv_file_path, index=False)
 
-# Conversion to CSV
-data.to_dataframe().reset_index().to_csv(f'output_{last_date}.csv', index=False)
+# Upload the CSV file to S3
+try:
+    s3_client.upload_file(csv_file_path, bucket_name, os.path.join(s3_folder, os.path.basename(csv_file_path)))
+    print(f"File '{csv_file_path}' uploaded to S3 bucket '{bucket_name}' in folder '{s3_folder}'.")
+except Exception as e:
+    print(f"Error uploading file to S3: {e}")
 
-print("Data downloaded in file", f'output_{last_date}.csv', "for the last date available in the dataset:", last_date)
+# Clean up: Delete the local CSV file
+os.remove(csv_file_path)
